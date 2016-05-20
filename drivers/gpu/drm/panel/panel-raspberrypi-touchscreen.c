89a633b5470a95a3a78c02e90eea8b976d138fc5
Eric Anholt <eric@anholt.net>
2016-05-20 11:52:44 -0700

    panel-regs-rewrite

--- drivers/gpu/drm/panel/panel-raspberrypi-touchscreen.c
+++ drivers/gpu/drm/panel/panel-raspberrypi-touchscreen.c
@@ -90,14 +90,10 @@ enum REG_ADDR {
 #define CLW_DPHYCONTRX		0x0020
 #define D0W_DPHYCONTRX		0x0024
 #define D1W_DPHYCONTRX		0x0028
-#define D2W_DPHYCONTRX		0x002C
-#define D3W_DPHYCONTRX		0x0030
 #define COM_DPHYCONTRX		0x0038
 #define CLW_CNTRL		0x0040
 #define D0W_CNTRL		0x0044
 #define D1W_CNTRL		0x0048
-#define D2W_CNTRL		0x004C
-#define D3W_CNTRL		0x0050
 #define DFTMODE_CNTRL		0x0054
 
 /* DSI PPI Layer Registers */
@@ -105,32 +101,22 @@ enum REG_ADDR {
 #define PPI_BUSYPPI		0x0108
 #define PPI_LINEINITCNT		0x0110
 #define PPI_LPTXTIMECNT		0x0114
-#define PPI_LANEENABLE		0x0134
-#define PPI_TX_RX_TA		0x013C
+//#define PPI_LANEENABLE		0x0134
+//#define PPI_TX_RX_TA		0x013C
 #define PPI_CLS_ATMR		0x0140
 #define PPI_D0S_ATMR		0x0144
 #define PPI_D1S_ATMR		0x0148
-#define PPI_D2S_ATMR		0x014C
-#define PPI_D3S_ATMR		0x0150
 #define PPI_D0S_CLRSIPOCOUNT	0x0164
 #define PPI_D1S_CLRSIPOCOUNT	0x0168
-#define PPI_D2S_CLRSIPOCOUNT	0x016C
-#define PPI_D3S_CLRSIPOCOUNT	0x0170
 #define CLS_PRE			0x0180
 #define D0S_PRE			0x0184
 #define D1S_PRE			0x0188
-#define D2S_PRE			0x018C
-#define D3S_PRE			0x0190
 #define CLS_PREP		0x01A0
 #define D0S_PREP		0x01A4
 #define D1S_PREP		0x01A8
-#define D2S_PREP		0x01AC
-#define D3S_PREP		0x01B0
 #define CLS_ZERO		0x01C0
 #define D0S_ZERO		0x01C4
 #define D1S_ZERO		0x01C8
-#define D2S_ZERO		0x01CC
-#define D3S_ZERO		0x01D0
 #define PPI_CLRFLG		0x01E0
 #define PPI_CLRSIPO		0x01E4
 #define HSTIMEOUT		0x01F0
@@ -140,66 +126,74 @@ enum REG_ADDR {
 #define DSI_STARTDSI		0x0204
 #define DSI_BUSYDSI		0x0208
 #define DSI_LANEENABLE		0x0210
+# define DSI_LANEENABLE_CLOCK		BIT(0)
+# define DSI_LANEENABLE_D0		BIT(1)
+# define DSI_LANEENABLE_D1		BIT(2)
+
 #define DSI_LANESTATUS0		0x0214
 #define DSI_LANESTATUS1		0x0218
 #define DSI_INTSTATUS		0x0220
 #define DSI_INTMASK		0x0224
 #define DSI_INTCLR		0x0228
 #define DSI_LPTXTO		0x0230
+#define DSI_MODE		0x0260
+#define DSI_PAYLOAD0		0x0268
+#define DSI_PAYLOAD1		0x026C
+#define DSI_SHORTPKTDAT		0x0270
+#define DSI_SHORTPKTREQ		0x0274
+#define DSI_BTASTA		0x0278
+#define DSI_BTACLR		0x027C
 
 /* DSI General Registers */
 #define DSIERRCNT		0x0300
+#define DSISIGMOD		0x0304
 
 /* DSI Application Layer Registers */
 #define APLCTRL			0x0400
-#define RDPKTLN			0x0404
+#define APLSTAT			0x0404
+#define APLERR			0x0408
+#define PWRMOD			0x040C
+#define RDPKTLN			0x0410
+#define PXLFMT			0x0414
+#define MEMWRCMD		0x0418
 
-/* Video Path Registers */
-#define VPCTRL			0x0450
-#define HTIM1			0x0454
-#define HTIM2			0x0458
-#define VTIM1			0x045C
-#define VTIM2			0x0460
-#define VFUEN			0x0464
-
-/* from anholt */
+/* LCDC/DPI Host Registers */
 #define LCDCTRL			0x0420
-#define SYSCTRL			0x0464
+#define HSR			0x0424
+#define HDISPR			0x0428
+#define VSR			0x042C
+#define VDISPR			0x0430
+#define VFUEN			0x0434
+
+/* DBI-B Host Registers */
+#define DBIBCTRL		0x0440
+
+/* SPI Master Registers */
 #define SPICMR			0x0450
+#define SPITCR			0x0454
 
-/* LVDS Registers */
-#define LVMX0003		0x0480
-#define LVMX0407		0x0484
-#define LVMX0811		0x0488
-#define LVMX1215		0x048C
-#define LVMX1619		0x0490
-#define LVMX2023		0x0494
-#define LVMX2427		0x0498
-#define LVCFG			0x049C
-#define LVPHY0			0x04A0
-#define LVPHY1			0x04A4
-
-/* System Registers */
-#define SYSSTAT			0x0500
-#define SYSRST			0x0504
+/* System Controller Registers */
+#define SYSSTAT			0x0460
+#define SYSCTRL			0x0464
+#define SYSPLL1			0x0468
+#define SYSPLL2			0x046C
+#define SYSPLL3			0x0470
+#define SYSPMCTRL		0x047C
 
 /* GPIO Registers */
-/*#define GPIOC			0x0520*/
-#define GPIOO			0x0524
-#define GPIOI			0x0528
+#define GPIOC			0x0480
+#define GPIOO			0x0484
+#define GPIOI			0x0488
 
 /* I2C Registers */
-#define I2CTIMCTRL		0x0540
-#define I2CMADDR		0x0544
-#define WDATAQ			0x0548
-#define RDATAQ			0x054C
+#define I2CCLKCTRL		0x0490
 
 /* Chip/Rev Registers */
-#define IDREG			0x0580
+#define IDREG			0x04A0
 
 /* Debug Registers */
-#define DEBUG00			0x05A0
-#define DEBUG01			0x05A4
+#define WCMDQUEUE		0x0500
+#define RCMDQUEUE		0x0504
 
 struct rpi_touchscreen {
 	struct drm_panel base;
@@ -345,7 +339,10 @@ static int rpi_touchscreen_enable(struct drm_panel *panel)
 			break;
 	}
 
-	rpi_touchscreen_write(ts, PPI_LANEENABLE, lanes == 2 ? 0x07 : 0x03);
+	rpi_touchscreen_write(ts, DSI_LANEENABLE,
+			      DSI_LANEENABLE_CLOCK |
+			      DSI_LANEENABLE_D0 |
+			      (ts->dsi->lanes > 1 ? DSI_LANEENABLE_D1 : 0));
 	rpi_touchscreen_write(ts, PPI_D0S_CLRSIPOCOUNT, 0x05);
 	rpi_touchscreen_write(ts, PPI_D1S_CLRSIPOCOUNT, 0x05);
 	rpi_touchscreen_write(ts, PPI_D0S_ATMR, 0x00);
