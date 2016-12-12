a439d3954c7b4ad8df474405935a2cdc5eacf37f
Eric Anholt <eric@anholt.net>
2016-12-12 11:59:56 -0800

    panel: Use I2C for our writes
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/panel/panel-raspberrypi-touchscreen.c
+++ drivers/gpu/drm/panel/panel-raspberrypi-touchscreen.c
@@ -252,6 +252,7 @@ static void rpi_touchscreen_i2c_write(struct rpi_touchscreen *ts, u8 reg, u8 val
 
 static int rpi_touchscreen_write(struct rpi_touchscreen *ts, u16 reg, u32 val)
 {
+#if 0
 	u8 msg[] = {
 		reg,
 		reg >> 8,
@@ -264,6 +265,12 @@ static int rpi_touchscreen_write(struct rpi_touchscreen *ts, u16 reg, u32 val)
 	dev_info(ts->base.dev, "W 0x%04x -> 0x%08x\n", reg, val);
 
 	mipi_dsi_dcs_write_buffer(ts->dsi, msg, sizeof(msg));
+#endif
+
+	rpi_touchscreen_i2c_write(ts, REG_WR_ADDRH, reg >> 8);
+	rpi_touchscreen_i2c_write(ts, REG_WR_ADDRL, reg);
+	rpi_touchscreen_i2c_write(ts, REG_WRITEH, val >> 8);
+	rpi_touchscreen_i2c_write(ts, REG_WRITEL, val);
 
 	return 0;
 }
