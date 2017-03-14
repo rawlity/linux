484e3c4ab5c405e1993bb782b252898d8d664a1c
Eric Anholt <eric@anholt.net>
2017-03-14 13:15:26 -0700

    pl111: Switch set_config to atomic
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/pl111/pl111_drm_crtc.c
+++ drivers/gpu/drm/pl111/pl111_drm_crtc.c
@@ -216,7 +216,7 @@ static void pl111_disable_vblank(struct drm_crtc *crtc)
 }
 
 const struct drm_crtc_funcs crtc_funcs = {
-	.set_config = drm_crtc_helper_set_config,
+	.set_config = drm_atomic_helper_set_config,
 	.page_flip = drm_atomic_helper_page_flip,
 	.reset = drm_atomic_helper_crtc_reset,
 	.destroy = pl111_crtc_destroy,
