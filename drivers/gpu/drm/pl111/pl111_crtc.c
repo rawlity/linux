0c6ce8d0ae70c023d636a77fcce742d41d52342e
Eric Anholt <eric@anholt.net>
2017-03-16 14:00:52 -0700

    pl111: Fire page flip events immediately when disabling
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/pl111/pl111_crtc.c
+++ drivers/gpu/drm/pl111/pl111_crtc.c
@@ -156,7 +156,7 @@ static void pl111_crtc_helper_atomic_flush(struct drm_crtc *crtc,
 		crtc->state->event = NULL;
 
 		spin_lock_irq(&crtc->dev->event_lock);
-		if (drm_crtc_vblank_get(crtc) == 0)
+		if (crtc->state->active && drm_crtc_vblank_get(crtc) == 0)
 			drm_crtc_arm_vblank_event(crtc, event);
 		else
 			drm_crtc_send_vblank_event(crtc, event);
