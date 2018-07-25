e60a573a283ccfbcd53d50d542aa2ea6dd5571e7
Eric Anholt <eric@anholt.net>
2018-07-25 11:09:52 -0700

    drm/v3d: Remove DRM_AUTH requirement from CL submit now that we use GMP
    Client BOs are now hidden from each other.  Note that HW state is
    still left intact between jobs, including TLB contents (roughly 32x32
    of the last pixels rendered, unless the client set had a color clear
    in their frame) and register state.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/v3d/v3d_drv.c
+++ drivers/gpu/drm/v3d/v3d_drv.c
@@ -284,13 +284,8 @@ static const struct file_operations v3d_drm_fops = {
 	.llseek = noop_llseek,
 };
 
-/* DRM_AUTH is required on SUBMIT_CL for now, while we don't have GMP
- * protection between clients.  Note that render nodes would be be
- * able to submit CLs that could access BOs from clients authenticated
- * with the master node.
- */
 static const struct drm_ioctl_desc v3d_drm_ioctls[] = {
-	DRM_IOCTL_DEF_DRV(V3D_SUBMIT_CL, v3d_submit_cl_ioctl, DRM_RENDER_ALLOW | DRM_AUTH),
+	DRM_IOCTL_DEF_DRV(V3D_SUBMIT_CL, v3d_submit_cl_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_WAIT_BO, v3d_wait_bo_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_CREATE_BO, v3d_create_bo_ioctl, DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(V3D_MMAP_BO, v3d_mmap_bo_ioctl, DRM_RENDER_ALLOW),
